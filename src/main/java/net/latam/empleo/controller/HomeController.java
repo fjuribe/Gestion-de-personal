5bcf1bf4074e86eb5d118918246a14acb16ca9e3
Javier1890 <sk90javiergamer16@gmail.com>
2019-12-19 20:54:43 -0300

    Agregando tipo de dato List<Vacante> al modelo y desplegar en una tabla HTML

--- src/main/java/net/latam/empleo/controller/HomeController.java
+++ src/main/java/net/latam/empleo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.latam.empleo.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +14,17 @@ import net.latam.empleo.model.Vacante;
 @Controller
 public class HomeController {
     
+	@GetMapping("/tabla")
+	public String mostrarTabla(Model model) {
+		List<Vacante> lista=getVacantes();
+		model.addAttribute("vacantes",lista);
+		
+		return "tabla";
+	}
+	
+	
+	
+	
 	@GetMapping("/detalle")
 	public String mostrarDetalle(Model model) {
 		Vacante vacante=new Vacante();
@@ -55,7 +67,56 @@ public class HomeController {
 		model.addAttribute("fecha", fechaPub);
 		model.addAttribute("salario",salario);
 		model.addAttribute("vigente", vigente);
-		return "home";
-		
+		return "home";	
+	}
+	
+	
+	/**
+	 * metodo que regresa una lista de objetos de tipo vacante
+	 */
+	private List<Vacante> getVacantes(){
+		SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
+		List<Vacante> lista=new LinkedList<>();
+		try {
+			Vacante vacante1=new Vacante();
+			vacante1.setId(1);
+			vacante1.setNombre("Ingeniero Civil");
+			vacante1.setDescripcion("solicitamos Ing. civil para dseñar aeropuerto");
+			vacante1.setFecha(sdf.parse("08-02-2019"));
+			vacante1.setSalario(8500.0);
+			
+			
+			Vacante vacante2=new Vacante();
+			vacante2.setId(2);
+			vacante2.setNombre("Contador Auditor");
+			vacante2.setDescripcion("Empresa gubernamental solicita director de finanzas de profesion Contador Auditor");
+			vacante2.setFecha(sdf.parse("09-02-2019"));
+			vacante2.setSalario(7500.0);
+			
+			
+			Vacante vacante3=new Vacante();
+			vacante3.setId(3);
+			vacante3.setNombre("ingeniero Civil Electrico");
+			vacante3.setDescripcion("Empresa internacional solicita analista energetico de potencias para proyecto maritimo");
+			vacante3.setFecha(sdf.parse("11-02-2019"));
+			vacante3.setSalario(9900.0);
+			
+			Vacante vacante4=new Vacante();
+			vacante4.setId(4);
+			vacante4.setNombre("Periodista");
+			vacante4.setDescripcion("Importante canal de television solicita periodista con poca experiencia laboral para integrarlo a su equipo de reportes");
+			vacante4.setFecha(sdf.parse("12-02-2019"));
+			vacante4.setSalario(9900.0);
+			
+			
+			lista.add(vacante1);
+			lista.add(vacante2);
+			lista.add(vacante3);
+			lista.add(vacante4);
+			
+		} catch (Exception e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+		return lista;
 	}
 }
