c4f10e5c7ab31bda17d5accef7c343161f022289
fjuribe <fjuribeui@gmail.com>
2020-01-13 10:32:19 -0300

    Formulario HTML para crear una vacante

--- src/main/java/net/latam/empleo/controller/VacantesController.java
+++ src/main/java/net/latam/empleo/controller/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,29 @@ public class VacantesController {
 
 	@Autowired
 	private IVacanteService serviceVacantes;
+	
+	
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacantes";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion,@RequestParam("categoria") int categoria,
+			@RequestParam("estatus") String estatus,@RequestParam("fecha") String fecha,@RequestParam("destacado") int destacado,@RequestParam("salario") double salario,
+			@RequestParam("detalles") String detalles) {
+		
+		System.out.println("Nombre vacante:"+nombre);
+		System.out.println("Descripción:"+descripcion);
+		System.out.println("Categoria:"+categoria);
+		System.out.println("Estatus:"+estatus);
+		System.out.println("Fecha Publicación:"+fecha);
+		System.out.println("Destacado:"+destacado);
+		System.out.println("Salario Ofrecido:"+salario);
+		System.out.println("Detalles:"+detalles);
+		
+		return "vacantes/listVacantes";
+	}
 	
 	@GetMapping("/delete")
 	public String eliminar(@RequestParam("id") int idVacante,Model model) {
