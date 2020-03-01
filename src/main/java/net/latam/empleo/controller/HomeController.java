6563a71c0cdd91cd641f478f6010d746c06fbb0a
fjuribe <fjuribeui@gmail.com>
2020-03-01 19:19:49 -0300

    Agregando Query By eample(ExampleMatcher) para uar el operador like

--- src/main/java/net/latam/empleo/controller/HomeController.java
+++ src/main/java/net/latam/empleo/controller/HomeController.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -92,7 +93,12 @@ public class HomeController {
 	@GetMapping("/search")
 	public String buscar(@ModelAttribute("search") Vacante vacante,Model model) {
 		System.out.println("buscando por:"+vacante);
-		Example<Vacante> example=Example.of(vacante);
+		
+		ExampleMatcher matcher=ExampleMatcher.matching().
+				//where descripcion line '%?%'
+				withMatcher("descripcion", ExampleMatcher.GenericPropertyMatchers.contains());
+		
+		Example<Vacante> example=Example.of(vacante,matcher);
 		List<Vacante> lista=serviceVacantes.buscarByExample(example);
 		model.addAttribute("vacantes", lista);
 		return "home";
