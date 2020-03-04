c8801d434f2c30b3afa2e3c3a3035a3e889db081
fjuribe <fjuribeui@gmail.com>
2020-03-04 20:59:48 -0300

    Agregar expresion sec:authorize de thymeleaf - renderizar el menu dinamico

--- src/main/java/net/latam/empleo/security/DatabaseWebSecurity.java
+++ src/main/java/net/latam/empleo/security/DatabaseWebSecurity.java
@@ -35,6 +35,12 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 				// Las vistas públicas no requieren autenticación
 				.antMatchers("/", "/signup", "/search", "/vacantes/view/**").permitAll()
 				
+				// Asignar permisos a URLs por ROLES
+				.antMatchers("/vacantes/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
+				.antMatchers("/categorias/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
+				.antMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
+				
+				
 				// Todas las demás URLs de la Aplicación requieren autenticación
 				.anyRequest().authenticated()
 				
