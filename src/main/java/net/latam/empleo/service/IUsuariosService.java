a505c935be326328a58b40cde20aadab4e7f7029
fjuribe <fjuribeui@gmail.com>
2020-03-05 16:15:46 -0300

    Hay un bug , ya que no retorna el user

--- src/main/java/net/latam/empleo/service/IUsuariosService.java
+++ src/main/java/net/latam/empleo/service/IUsuariosService.java
@@ -22,6 +22,7 @@ public interface IUsuariosService {
 	
 	// Ejercicio: Implementar método que recupera todos los usuarios. Usar vista de listUsuarios.html
 	List<Usuario> buscarTodos();
+	
 	Usuario buscarPorUsername(String username);
 }
 
